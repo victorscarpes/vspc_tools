8bfc91a793ce7ee1c9c8cf0c47af596b8922888f
Victor <79273705+victorscarpes@users.noreply.github.com>
2022-06-29 16:00:25 -0300

    Adição dos arquivos iniciais

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="vspc_tools",
+      version="1.0",
+      description="Diversas funções que utilizo o tempo inteiro",
+      author="Victor Sabiá Pereira Carpes",
+      author_email="",
+      url="",
+      install_requires=["to_precision @ git+https://bitbucket.org/william_rusnack/to-precision.git"]
+      )
