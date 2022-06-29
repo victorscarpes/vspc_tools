fdf6a135f7d059c66e7a26c9605516019934eb5c
victorscarpes <victorscarpes@gmail.com>
2022-06-29 17:09:25 -0300

    Atualização da versão no setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup, find_packages
 
 setup(name="vspc_tools",
-      version="1.0",
+      version="1.0.1",
       description="Diversas funções que utilizo o tempo inteiro",
       author="Victor Sabiá Pereira Carpes",
       url="https://github.com/victorscarpes/vspc_tools",
