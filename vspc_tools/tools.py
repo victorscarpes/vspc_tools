0bdd4739d584b8e369583002d8a1463a3a0419b9
victorscarpes <victorscarpes@gmail.com>
2022-07-01 15:32:19 -0300

    Substituição de 10¹ por 10 na função sci_notation

--- vspc_tools/tools.py
+++ vspc_tools/tools.py
@@ -62,7 +62,7 @@ def sci_notation(value:float, unit:str, precision:int, decimal:str=',') -> str:
     string = string.replace('.E', 'E').replace('.', decimal)
     mantissa, exponent = string.split('E')
     simbs = ['-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    supers = ['⁻', '⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹']
+    supers = ['⁻', '⁰', '', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹']
     if exponent == '0':
         return mantissa + unit
     else:
