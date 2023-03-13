27097330dd0d9d96bbda33464f324c4514178ac8
victorscarpes <victorscarpes@gmail.com>
2023-03-13 22:43:43 +0100

    Removida dependência de to_precision

--- setup.py
+++ setup.py
@@ -5,6 +5,5 @@ setup(name="vspc_tools",
       description="Diversas funções que utilizo o tempo inteiro",
       author="Victor Sabiá Pereira Carpes",
       url="https://github.com/victorscarpes/vspc_tools",
-      install_requires=["to_precision @ git+https://bitbucket.org/william_rusnack/to-precision.git"],
       packages=find_packages()
       )
